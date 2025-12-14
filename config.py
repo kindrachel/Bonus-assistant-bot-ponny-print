661ddf6149f32f4c6ddbc043bdb39bbaff1fa998
antbled <120652266+kindrachel@users.noreply.github.com>
2025-12-14 17:10:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ REFERRAL_POINTS = 500  # Баллы за приглашение
 NEW_USER_POINTS = 500   # Баллы новому пользователю
 STARTPOINTS = 250 #Приветственные бонусы
 
-ADMIN_IDS = [5895398369, 656832643] #tg_id админов
+
+ADMIN_IDS = [] #tg_id админов
